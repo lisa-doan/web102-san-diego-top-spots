89fce486fe1d2a45f12693fb0f3ff3d105d7e1db
Lisa Doan <ldoan24@gmail.com>
2021-10-20 16:27:16 -0700

    Completed web102-san-diego-top-spots

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
 // write your code here
-
+ $.getJSON("data.json", function(result){
+        $.each(result, function(i, field){
+            let $tr = $("<tr>").append(
+            $("<td>").text(field.name),
+            $("<td>").text(field.description),
+            $("<td>").append(
+            `<a href='https://www.google.com/maps?q=${field.location[0]},${field.location[1]}' target='_blank'>Open</a>`
+            ));
+            $("table").append($tr);
+        });
+    });
 });
